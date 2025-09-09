8da13cfcc3d658bcf66c089354fbce66d6df58e9
Vittorviehper <132918495+Vittorviehper@users.noreply.github.com>
2025-09-09 09:09:57 -0300

    Add files via upload

---
+++ segundo/oficinas/script.js
@@ -0,0 +1,24 @@
+document.getElementById("btn1").onclick = function() {
+    window.location.href = "https://exemplo.com/oficina1";
+};
+document.getElementById("btn2").onclick = function() {
+    window.location.href = "https://exemplo.com/oficina2";
+};
+document.getElementById("btn3").onclick = function() {
+    window.location.href = "https://exemplo.com/oficina3";
+};
+document.getElementById("btn4").onclick = function() {
+    window.location.href = "https://exemplo.com/oficina4";
+};
+document.getElementById("btn5").onclick = function() {
+    window.location.href = "https://exemplo.com/oficina5";
+};
+document.getElementById("btn6").onclick = function() {
+    window.location.href = "https://exemplo.com/oficina6";
+};
+document.getElementById("btn7").onclick = function() {
+    window.location.href = "https://exemplo.com/oficina7";
+};
+document.getElementById("btn8").onclick = function() {
+    window.location.href = "https://exemplo.com/oficina8";
+};
